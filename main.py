d6ed6c32f384c2e679e14d06167ede8f5c2d4a7c
Kail J <68143465+KailJ1@users.noreply.github.com>
2025-04-08 22:35:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 
 
 def display_txt_files():
-    # Получаем путь к текущей директории
-    current_directory = os.path.dirname(os.path.realpath(__file__))
+    # Указываем путь к целевой директории
+    target_directory = '/home/mayor/Documents/kapibarovsk/'
 
-    # Проходим по всем файлам в текущей директории
-    for filename in os.listdir(current_directory):
+    # Проходим по всем файлам в указанной директории
+    for filename in os.listdir(target_directory):
         # Если файл имеет расширение .txt
         if filename.endswith('.txt'):
-            file_path = os.path.join(current_directory, filename)
+            file_path = os.path.join(target_directory, filename)
             print(f"Содержимое файла {filename}:")
             # Открываем и выводим содержимое файла
             with open(file_path, 'r') as file:
